6b17d67df13ee3d1dba957353984f2e55613feb9
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-10-30 13:32:53 +0900

    feat: create sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+/* lib */
+import axios from "axios";
+import { useQuery } from "react-query";
+/* types */
+import { User } from "./types";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const {isLoading, isError, data} = useQuery("users", () => {
+    return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.data);
+  });
+
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    if (!isLoading) {
+      setUsers(data);
+    }
+
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    console.log(`isError: ${isError}`);
+  }, [isError]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users List</h1>
+      {users.map((user: User) =>
+        <div key={user.id}>
+          <h3>name: {user.name}</h3>
+          <div>username: {user.username}</div>
+          <div>phone: {user.phone}</div>
+          <div>website: {user.website}</div>
+        </div>
+      )}
     </div>
   );
+
 }
 
 export default App;
